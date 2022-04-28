40f2fe22cabb3d89df4ccda8c327b075889d4e09
Aleksey <11_alx_11@ro.ru>
2022-04-28 14:06:57 +0300

    init z1

---
+++ labrab3/z1/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+int main()
+{
+    stack<string> tags;
+
+    cout << "Enter HTML code (to exit input mode, type 'e!'):\n";
+    string tmp, tag;
+    while (tmp != "e!") {
+        getline(cin, tmp);
+
+    }
+
+    return 0;
+}
