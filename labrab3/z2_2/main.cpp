faad99aaaf43b4eaf812bb7206dbf5a29cb6d59d
Aleksey <11_alx_11@ro.ru>
2022-05-12 09:58:26 +0300

    fix list to queue

--- labrab3/z2_2/main.cpp
+++ labrab3/z2_2/main.cpp
@@ -1,63 +1,59 @@
 #include <iostream>
-#include <list>
+#include <queue>
 
 using namespace std;
 
 int main()
 {
-    const int INFINITY = 30;
-    
-    //We have 2 lists with elements (we shall define elements as int)
-    list<int> list1;
-    list<int> list2;
-    list<int> listOut;
-    
+    //We have 2 queues with elements (we shall define elements as int)
+    queue<int> queue1;
+    queue<int> queue2;
+    queue<int> queueOut;
+
     for (int i=0; i<20; i++) {
         if (!(i%3)) {
-            //0 2 4 6...
-            list1.push_back(i);
+            //0 3 6 9 12 15 18
+            queue1.push(i);
         }
         else {
-            //1 3 5 7...
-            list2.push_back(i);
+            //1 2 4 5 7 8 10 11 13 14 16 17 19
+            queue2.push(i);
         }
     }
-    
-    int minimum;
-    auto list1_idx = list1.begin();
-    auto list2_idx = list2.begin();
-    int list1_elem = *list1_idx;
-    int list2_elem = *list2_idx;
 
-    while (list1_idx != list1.end() || list2_idx != list2.end()) {
-        if (list1_elem < list2_elem) {
-            minimum = list1_elem;
-            list1_idx++;
-            if (list1_idx == list1.end()) {
-                list1_elem = INFINITY;
-            }
-            else {
-                list1_elem = *list1_idx;
-            }
+    int minimum;
+    while (!queue1.empty() || !queue2.empty()) {
+        if (queue1.front() < queue2.front()) {
+            minimum = queue1.front();
+            queue1.pop();
         }
         else {
-            minimum = list2_elem;
-            list2_idx++;
-            if (list2_idx == list2.end()) {
-                list2_elem = INFINITY;
-            }
-            else {
-                list2_elem = *list2_idx;
+            minimum = queue2.front();
+            queue2.pop();
+        }
+
+        queueOut.push(minimum);
+
+        if (queue1.empty()) {
+            while (!queue2.empty()) {
+                queueOut.push(queue2.front());
+                queue2.pop();
             }
         }
-        
-        listOut.push_back(minimum);
+        if (queue2.empty()) {
+            while (!queue1.empty()) {
+                queueOut.push(queue1.front());
+                queue1.pop();
+            }
+        }
     }
-    
-    for (auto elem : listOut) {
-        cout << (elem) << " ";
+
+    //0 1 2 3 4 5...
+    for (int i=0; i<20; i++) {
+        cout << queueOut.front() << " ";
+        queueOut.pop();
     }
     cout << endl;
-    
+
     return 0;
 }
