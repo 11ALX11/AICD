ea5c08f20c8a4fcbd8d94f625fe681691738291c
Aleksey <11_alx_11@ro.ru>
2022-05-12 11:13:11 +0300

    released bstree, hashtab

---
+++ labrab4/hashtab.h
@@ -0,0 +1,26 @@
+#ifndef TASK_HASHTAB_H
+#define TASK_HASHTAB_H
+
+#include <stdlib.h>
+#include <string.h>
+
+#define HASHTAB_SIZE 200000
+#define HASHTAB_MUL 31
+
+struct listnode {
+    char* key;
+    int value;
+    struct listnode* next;
+};
+
+int hash(char *key);
+
+void hashtab_init(struct listnode hashtab);
+
+void hashtab_add(struct listnode hashtab, char *key, int value);
+
+struct listnode hashtab_lookup(struct listnode hashtab, char *key);
+
+void hashtab_delete(struct listnode **hashtab, char *key);
+
+#endif
---
+++ labrab4/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "bstree.h"
+#include "hashtab.h"
+
+#define HASHTAB_SIZE 200000
+
+using namespace std;
+
+int main()
+{
+    struct listnode* hashtab = new struct listnode[HASHTAB_SIZE];
+
+    cout << "Hello world!" << endl;
+    return 0;
+}
