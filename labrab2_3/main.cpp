a77c0f15c4b84190ec9b272e207ccea1f719129d
Aleksey <11_alx_11@ro.ru>
2022-12-27 14:27:06 +0300

    working on lr3

--- labrab2_3/main.cpp
+++ labrab2_3/main.cpp
@@ -6,8 +6,16 @@ vector<pair<int, int>> graph[100000];
 vector<pair<int, pair<int, int>>> edges;
 int INF;
 
+int p[100000];
+int rk[100000];
+
 void print_graf(int m);
 void kruskal(int n, int m);
+void prima();
+
+bool kruskal_merge(int a, int b);
+int kruskal_dsu_get_root(int v);
+
 bool cmp(auto a, auto b);
 
 int main() {
@@ -37,12 +45,12 @@ int main() {
     infile.close();
     //we done with reading graph
 
-    kruskal(n, m);
     cout << "graph:\n";
     print_graf(m);
     cout << "Infinity is " << INF << endl;
 
     kruskal(n, m);
+    prima();
 
     return 0;
 }
@@ -60,7 +68,82 @@ void kruskal(int n, int m) {
         return a.first < b.first;
     });
 
-    for (pair<int, pair<int, int>> edge: edges) {
-        //...
+    int ost_weight = 0;
+
+    for (int i = 0; i < 100000; i++) {
+        p[i] = i;
+        rk[i] = 1;
     }
+
+    for (pair<int, pair<int, int>> edge: edges) {
+        if (kruskal_merge(edge.second.first, edge.second.second)) {
+            ost_weight += edge.first;
+            cout << edge.second.first+1 << " - " << edge.second.second+1 << endl;
+        }
+    }
+
+    cout << "Min ostov weight: " << ost_weight << endl;
+}
+
+bool kruskal_merge(int a, int b) {
+    int ra = kruskal_dsu_get_root(a), rb = kruskal_dsu_get_root(b);
+
+    if (ra == rb) {
+        return false;
+    } else {
+        if (rk[ra] < rk[rb]) {
+            p[ra] = rb;
+        } else if (rk[rb] < rk[ra]) {
+            p[rb] = ra;
+        } else {
+            p[ra] = rb;
+            rk[rb]++;
+        }
+
+        return true;
+    }
+}
+
+int kruskal_dsu_get_root(int v) {
+    if (p[v] == v) {
+        return v;
+    }
+    else {
+        return p[v] = kruskal_dsu_get_root(p[v]);
+    }
+}
+
+bool used[100000];
+
+void prima() {
+
+    int ost_weight = 0;
+
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> q;
+
+    q.push({0, 0});
+
+    while (!q.empty()) {
+        pair<int, int> c = q.top();
+        q.pop();
+
+        int dst = c.first, v = c.second;
+
+        if (used[v]) {
+            continue;
+        }
+
+        used[v] = true;
+        ost_weight += dst;
+
+        for (pair<int, int> e: graph[v]) {
+            int u = e.first, len_vu = e.second;
+
+            if (!used[u]) {
+                q.push({len_vu, u});
+            }
+        }
+    }
+
+    cout << "Min ostov weight: " << ost_weight << endl;
 }
